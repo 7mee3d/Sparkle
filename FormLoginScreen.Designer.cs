74ecede763df0ae017baa1b5f3d7e715c5780681
Ahmed Jehad <alanturing404@gmail.com>
2025-10-07 00:52:07 +0300

    Add the Logic Login in the Sparkle
    Change the Hover Text Box
    
    Change the Color Hover State Text Box

--- FormLoginScreen.Designer.cs
+++ FormLoginScreen.Designer.cs
@@ -32,10 +32,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLoginScreen));
             this.GBorderLessFormRaduis = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
             this.MainPanelShadowLogin = new Guna.UI2.WinForms.Guna2ShadowPanel();
-            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.ButtonLoginTheSparkle = new Guna.UI2.WinForms.Guna2Button();
+            this.GTextBoxPasswordLogin = new Guna.UI2.WinForms.Guna2TextBox();
             this.GTextBoxUsernameLogin = new Guna.UI2.WinForms.Guna2TextBox();
             this.labelLoginWord = new System.Windows.Forms.Label();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.MainPanelShadowLogin.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,8 +49,8 @@
             // MainPanelShadowLogin
             // 
             this.MainPanelShadowLogin.BackColor = System.Drawing.Color.Transparent;
-            this.MainPanelShadowLogin.Controls.Add(this.guna2Button1);
-            this.MainPanelShadowLogin.Controls.Add(this.guna2TextBox2);
+            this.MainPanelShadowLogin.Controls.Add(this.ButtonLoginTheSparkle);
+            this.MainPanelShadowLogin.Controls.Add(this.GTextBoxPasswordLogin);
             this.MainPanelShadowLogin.Controls.Add(this.GTextBoxUsernameLogin);
             this.MainPanelShadowLogin.Controls.Add(this.labelLoginWord);
             this.MainPanelShadowLogin.FillColor = System.Drawing.Color.WhiteSmoke;
@@ -63,33 +63,60 @@
             this.MainPanelShadowLogin.TabIndex = 0;
             this.MainPanelShadowLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPanelShadowLogin_Paint);
             // 
-            // guna2TextBox2
+            // ButtonLoginTheSparkle
             // 
-            this.guna2TextBox2.Animated = true;
-            this.guna2TextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.guna2TextBox2.BorderRadius = 20;
-            this.guna2TextBox2.BorderThickness = 2;
-            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox2.DefaultText = "";
-            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.FillColor = System.Drawing.Color.WhiteSmoke;
-            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.SeaGreen;
-            this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(74)))), ((int)(((byte)(75)))));
-            this.guna2TextBox2.HoverState.FillColor = System.Drawing.Color.Transparent;
-            this.guna2TextBox2.IconLeft = global::Sparkle.Properties.Resources._lock;
-            this.guna2TextBox2.IconLeftOffset = new System.Drawing.Point(11, 0);
-            this.guna2TextBox2.IconLeftSize = new System.Drawing.Size(25, 25);
-            this.guna2TextBox2.Location = new System.Drawing.Point(98, 401);
-            this.guna2TextBox2.Name = "guna2TextBox2";
-            this.guna2TextBox2.PlaceholderText = "Password";
-            this.guna2TextBox2.SelectedText = "";
-            this.guna2TextBox2.Size = new System.Drawing.Size(338, 45);
-            this.guna2TextBox2.TabIndex = 1;
-            this.guna2TextBox2.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
+            this.ButtonLoginTheSparkle.Animated = true;
+            this.ButtonLoginTheSparkle.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
+            this.ButtonLoginTheSparkle.BorderRadius = 9;
+            this.ButtonLoginTheSparkle.BorderThickness = 2;
+            this.ButtonLoginTheSparkle.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
+            this.ButtonLoginTheSparkle.CustomBorderThickness = new System.Windows.Forms.Padding(2);
+            this.ButtonLoginTheSparkle.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.ButtonLoginTheSparkle.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.ButtonLoginTheSparkle.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.ButtonLoginTheSparkle.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.ButtonLoginTheSparkle.FillColor = System.Drawing.Color.Transparent;
+            this.ButtonLoginTheSparkle.Font = new System.Drawing.Font("Goudy Old Style", 9.75F, System.Drawing.FontStyle.Bold);
+            this.ButtonLoginTheSparkle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
+            this.ButtonLoginTheSparkle.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
+            this.ButtonLoginTheSparkle.HoverState.ForeColor = System.Drawing.Color.White;
+            this.ButtonLoginTheSparkle.Image = global::Sparkle.Properties.Resources.enter;
+            this.ButtonLoginTheSparkle.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.ButtonLoginTheSparkle.ImageOffset = new System.Drawing.Point(25, 0);
+            this.ButtonLoginTheSparkle.Location = new System.Drawing.Point(107, 490);
+            this.ButtonLoginTheSparkle.Name = "ButtonLoginTheSparkle";
+            this.ButtonLoginTheSparkle.Size = new System.Drawing.Size(161, 51);
+            this.ButtonLoginTheSparkle.TabIndex = 2;
+            this.ButtonLoginTheSparkle.Text = "Login";
+            this.ButtonLoginTheSparkle.Click += new System.EventHandler(this.ButtonLoginTheSparkle_Click);
+            // 
+            // GTextBoxPasswordLogin
+            // 
+            this.GTextBoxPasswordLogin.Animated = true;
+            this.GTextBoxPasswordLogin.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
+            this.GTextBoxPasswordLogin.BorderRadius = 20;
+            this.GTextBoxPasswordLogin.BorderThickness = 2;
+            this.GTextBoxPasswordLogin.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.GTextBoxPasswordLogin.DefaultText = "";
+            this.GTextBoxPasswordLogin.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.GTextBoxPasswordLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.GTextBoxPasswordLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.GTextBoxPasswordLogin.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.GTextBoxPasswordLogin.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.GTextBoxPasswordLogin.FocusedState.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.GTextBoxPasswordLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.GTextBoxPasswordLogin.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(74)))), ((int)(((byte)(75)))));
+            this.GTextBoxPasswordLogin.HoverState.FillColor = System.Drawing.Color.Transparent;
+            this.GTextBoxPasswordLogin.IconLeft = global::Sparkle.Properties.Resources._lock;
+            this.GTextBoxPasswordLogin.IconLeftOffset = new System.Drawing.Point(11, 0);
+            this.GTextBoxPasswordLogin.IconLeftSize = new System.Drawing.Size(25, 25);
+            this.GTextBoxPasswordLogin.Location = new System.Drawing.Point(107, 385);
+            this.GTextBoxPasswordLogin.Name = "GTextBoxPasswordLogin";
+            this.GTextBoxPasswordLogin.PlaceholderText = "Password";
+            this.GTextBoxPasswordLogin.SelectedText = "";
+            this.GTextBoxPasswordLogin.Size = new System.Drawing.Size(322, 45);
+            this.GTextBoxPasswordLogin.TabIndex = 1;
+            this.GTextBoxPasswordLogin.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
             // 
             // GTextBoxUsernameLogin
             // 
@@ -104,58 +131,31 @@
             this.GTextBoxUsernameLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.GTextBoxUsernameLogin.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
             this.GTextBoxUsernameLogin.FillColor = System.Drawing.Color.WhiteSmoke;
-            this.GTextBoxUsernameLogin.FocusedState.BorderColor = System.Drawing.Color.SeaGreen;
+            this.GTextBoxUsernameLogin.FocusedState.BorderColor = System.Drawing.Color.MediumSpringGreen;
             this.GTextBoxUsernameLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.GTextBoxUsernameLogin.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(74)))), ((int)(((byte)(75)))));
             this.GTextBoxUsernameLogin.HoverState.FillColor = System.Drawing.Color.Transparent;
             this.GTextBoxUsernameLogin.IconLeft = global::Sparkle.Properties.Resources.user__1_;
             this.GTextBoxUsernameLogin.IconLeftOffset = new System.Drawing.Point(11, 0);
             this.GTextBoxUsernameLogin.IconLeftSize = new System.Drawing.Size(25, 25);
-            this.GTextBoxUsernameLogin.Location = new System.Drawing.Point(98, 293);
+            this.GTextBoxUsernameLogin.Location = new System.Drawing.Point(107, 304);
             this.GTextBoxUsernameLogin.Name = "GTextBoxUsernameLogin";
             this.GTextBoxUsernameLogin.PlaceholderText = "Username\r\n";
             this.GTextBoxUsernameLogin.SelectedText = "";
-            this.GTextBoxUsernameLogin.Size = new System.Drawing.Size(338, 45);
+            this.GTextBoxUsernameLogin.Size = new System.Drawing.Size(322, 45);
             this.GTextBoxUsernameLogin.TabIndex = 1;
             this.GTextBoxUsernameLogin.TextChanged += new System.EventHandler(this.GTextBoxUsernameLogin_TextChanged);
             // 
             // labelLoginWord
             // 
             this.labelLoginWord.AutoSize = true;
-            this.labelLoginWord.Font = new System.Drawing.Font("Goudy Old Style", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLoginWord.Font = new System.Drawing.Font("Goudy Old Style", 65.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLoginWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.labelLoginWord.Location = new System.Drawing.Point(98, 126);
+            this.labelLoginWord.Location = new System.Drawing.Point(90, 142);
             this.labelLoginWord.Name = "labelLoginWord";
-            this.labelLoginWord.Size = new System.Drawing.Size(242, 91);
+            this.labelLoginWord.Size = new System.Drawing.Size(262, 99);
             this.labelLoginWord.TabIndex = 0;
             this.labelLoginWord.Text = "Log in";
-            // 
-            // guna2Button1
-            // 
-            this.guna2Button1.Animated = true;
-            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.guna2Button1.BorderRadius = 9;
-            this.guna2Button1.BorderThickness = 2;
-            this.guna2Button1.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.guna2Button1.CustomBorderThickness = new System.Windows.Forms.Padding(2);
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button1.Font = new System.Drawing.Font("Goudy Old Style", 9.75F, System.Drawing.FontStyle.Bold);
-            this.guna2Button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.guna2Button1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(92)))), ((int)(((byte)(94)))));
-            this.guna2Button1.HoverState.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Image = global::Sparkle.Properties.Resources.enter;
-            this.guna2Button1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.guna2Button1.ImageOffset = new System.Drawing.Point(25, 0);
-            this.guna2Button1.Location = new System.Drawing.Point(98, 509);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(161, 51);
-            this.guna2Button1.TabIndex = 2;
-            this.guna2Button1.Text = "Login";
-            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
             // 
             // FormLoginScreen
             // 
@@ -183,8 +183,8 @@
         private Guna.UI2.WinForms.Guna2ShadowPanel MainPanelShadowLogin;
         private System.Windows.Forms.Label labelLoginWord;
         private Guna.UI2.WinForms.Guna2TextBox GTextBoxUsernameLogin;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2TextBox GTextBoxPasswordLogin;
+        private Guna.UI2.WinForms.Guna2Button ButtonLoginTheSparkle;
     }
 }
 
--- FormLoginScreen.cs
+++ FormLoginScreen.cs
@@ -19,11 +19,107 @@ namespace Sparkle
       
         }
 
+        const string kPATH_FILE = "UsersInformation.txt";
+
+        struct stInformationUser
+        {
+            
+           public string stUsername;
+            public string stPassword; 
+
+        }
         private void ClearAlTextInTheTextBoxAfterClick(object sender)
         {
             Guna2TextBox GTextBox = (sender as Guna2TextBox  );
             GTextBox.Text = "";
 
+        }
+        private List<string> LoadLineInformationUsersFromFile ()
+        {
+            List<string> informationUserLine = new List<string>();
+
+            if (!System.IO.File.Exists(kPATH_FILE))
+            {
+                System.IO.File.Create(kPATH_FILE).Close(); 
+            }
+
+            System.IO.StreamReader ReadInformationAllUsers = new System.IO.StreamReader(kPATH_FILE);
+
+            string lineInformationOneUser = "";
+
+            while ((lineInformationOneUser = ReadInformationAllUsers.ReadLine()) != null)
+            {
+                informationUserLine.Add(lineInformationOneUser);
+                    
+                }
+
+            return informationUserLine; 
+
+        }
+
+        private List<string> SplitTheLineInformation ( string LineInformation ){
+
+            List<string> splitInformationLineToParts = new List<string>();
+
+            if (!string.IsNullOrEmpty(LineInformation))
+            {
+                splitInformationLineToParts.AddRange(LineInformation.Split(new string[]{ "||" }, StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            return splitInformationLineToParts;
+    }
+
+
+        private stInformationUser ConvertLineInformationUserToDataStruct (List<string> InformationUser)
+        {
+            stInformationUser informationUserOne = new stInformationUser();
+
+            informationUserOne.stUsername = InformationUser[0];
+            informationUserOne.stPassword = InformationUser[1];
+
+            return informationUserOne;
+
+        }
+
+        private List<stInformationUser> storeInformationAllUserst ()
+        {
+            List<string> InformationUserString = LoadLineInformationUsersFromFile();
+            List<stInformationUser> InformationAllUsersStruct = new List<stInformationUser>(); 
+
+            if (InformationUserString != null)
+            {
+                List <string> lineInfromationUser = new List<string>() ;
+                stInformationUser informationUserSt = new stInformationUser(); 
+
+                foreach(string lineInfoUser in InformationUserString)
+                {
+                    lineInfromationUser = SplitTheLineInformation(lineInfoUser);
+                    informationUserSt = ConvertLineInformationUserToDataStruct(lineInfromationUser);
+                    InformationAllUsersStruct.Add(informationUserSt);
+                }
+            }
+
+            return InformationAllUsersStruct; 
+        }
+
+        private bool areEqualSideStringCompare(string strOne , string strTwo)
+        {
+            return (strOne == strTwo);
+        }
+
+        private bool areFoundUsernameAndPassword(string username , string password )
+        {
+
+            List<stInformationUser> informationAllUsers = storeInformationAllUserst();
+
+            foreach (stInformationUser infoUser in informationAllUsers)
+            {
+                if (areEqualSideStringCompare(infoUser.stUsername, username) &&
+                    areEqualSideStringCompare(infoUser.stPassword, password)
+                    ) return true; 
+            }
+
+            return false; 
         }
         private void FormLoginScreen_Load(object sender, EventArgs e)
         {
@@ -54,6 +150,17 @@ namespace Sparkle
         private void MainPanelShadowLogin_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void ButtonLoginTheSparkle_Click(object sender, EventArgs e)
+        {
+            string username = GTextBoxUsernameLogin.Text;
+            string password = GTextBoxPasswordLogin.Text;
+
+            if (areFoundUsernameAndPassword(username, password))
+                MessageBox.Show("Login Sccessfully Sparkle", "Note Login");
+            else
+                MessageBox.Show("Faild Login Sccessfully Sparkle", "Note Login");
         }
     }
 }
