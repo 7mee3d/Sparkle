a4ce6a5944348dc99c12202236ce0a846dcd70cf
Ahmed Jehad <alanturing404@gmail.com>
2025-10-08 13:33:43 +0300

    Done of the write logic Check the ID is Exsits or not
    if the Exsits ID not save the file becouse diplicate the ID in the file and the ID is Uique غير متكرر
    
    this logic is inital Logic able to edit this logic ervery time

--- User Controls Sparkle/UserControlClients.cs
+++ User Controls Sparkle/UserControlClients.cs
@@ -13,7 +13,19 @@ namespace Sparkle.User_Controls_Sparkle
  
     public partial class UserControlClients : UserControl
     {
+        public UserControlClients()
+        {
+            InitializeComponent();
+        }
 
+        struct stInformationClient
+        {
+            public string stID; 
+            public string stName; 
+            public string stAddress; 
+            public string stEmail; 
+            public string stPhone; 
+        }
         const string kPATH_FILE_CLIENT = "InformationClients.txt";
 
         private string ConvertSeriseStringToLine(List<string> informationClient, string Separator = ";||;")
@@ -45,23 +57,112 @@ namespace Sparkle.User_Controls_Sparkle
             LoadInformationToFile.Close(); 
 
         }
-            public UserControlClients()
+     
+        private List <string> LoadAllLineInformationClientFromFile ()
         {
-            InitializeComponent();
+            List<string> LiInfromationAllClientsFromFile = new List<string>();
+
+            if (System.IO.File.Exists(kPATH_FILE_CLIENT))
+            {
+                System.IO.StreamReader readLineInformationFromFile = new System.IO.StreamReader(kPATH_FILE_CLIENT);
+
+                string lineInformationClient = "";
+
+                while ((lineInformationClient = readLineInformationFromFile.ReadLine()) != null)
+                {
+                    LiInfromationAllClientsFromFile.Add(lineInformationClient);
+                }
+
+                readLineInformationFromFile.Close(); 
+
+            }
+            return LiInfromationAllClientsFromFile;
         }
 
+        private List<string> SplitLineInformationOneClinet (string lineInformationClient)
+        {
+            List<string> informationOneClient = new List<string>();
+
+            if(lineInformationClient !=null)
+            {
+                informationOneClient.AddRange(lineInformationClient.Split(new String[] { ";||;" }, StringSplitOptions.RemoveEmptyEntries) ); 
+            }
+
+            return informationOneClient; 
+        }
+
+        private stInformationClient ConvertLineToDataInformationOneClient (List<string> informationOneClient )
+        {
+            stInformationClient infoClient = new stInformationClient();
+
+            if (informationOneClient.Count >= 5)
+            {
+
+                infoClient.stID = informationOneClient[0];
+                infoClient.stName = informationOneClient[1];
+                infoClient.stAddress = informationOneClient[2];
+                infoClient.stEmail = informationOneClient[3];
+                infoClient.stPhone = informationOneClient[4];
+
+            }
+
+            return infoClient;
+
+        }
+
+        private List<stInformationClient> LoadAllInformationToListStructure ()
+        {
+            List<string> AllLineInformationClient = LoadAllLineInformationClientFromFile(); 
+            List<stInformationClient> stAllInformationClients = new List<stInformationClient>() ; 
+
+            foreach (string lineInfoClient in AllLineInformationClient)
+            {
+                stInformationClient informatiomOneClinet = new stInformationClient(); 
+
+                List<string> SplitsLineToInformation = SplitLineInformationOneClinet(lineInfoClient);
+                informatiomOneClinet = ConvertLineToDataInformationOneClient(SplitsLineToInformation);
+                stAllInformationClients.Add(informatiomOneClinet);
+
+            }
+
+            return stAllInformationClients; 
+
+
+        }
+
+
+        private bool isIDExistsInFile(string ID)
+        {
+            List<stInformationClient> stAllInformationClients = LoadAllInformationToListStructure();
+
+            foreach (stInformationClient infoClient in stAllInformationClients)
+            {
+
+                if (infoClient.stID == ID) return true; 
+            }
+
+            return false; 
+        }
         private void ButtonAddNewClient_Click(object sender, EventArgs e)
         {
             List<string> LiInformationClient = new List<string>();
 
-            LiInformationClient.Add(GTextBoxIDClient.Text);
-            LiInformationClient.Add(GTextBoxNameClient.Text);
-            LiInformationClient.Add(GTextBoxAddressClient.Text);
-            LiInformationClient.Add(GTextBoxEmailClient.Text);
-            LiInformationClient.Add(GTextBoxPhoneClient.Text);
-
-            LoadLineInformationClientToFile(ConvertSeriseStringToLine(LiInformationClient, ";||;"));
-            MessageBox.Show("Done");
+            if (!isIDExistsInFile(GTextBoxIDClient.Text))
+            {
+                LiInformationClient.Add(GTextBoxIDClient.Text);
+                LiInformationClient.Add(GTextBoxNameClient.Text);
+                LiInformationClient.Add(GTextBoxAddressClient.Text);
+                LiInformationClient.Add(GTextBoxEmailClient.Text);
+                LiInformationClient.Add(GTextBoxPhoneClient.Text);
+                LoadLineInformationClientToFile(ConvertSeriseStringToLine(LiInformationClient, ";||;"));
+                MessageBox.Show("Done");
+            }
+            else
+            {
+                MessageBox.Show("This is ID Already Exsits in the List ", "Note The Add Clients", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+           
+        
         }
     }
 }
